2bab0ad3b303832ddc65636180ac5bb8a21cabc2
d3c0s4 <decosa.castro@gmail.com>
2019-05-11 00:37:45 +0200

    feat: galery page

---
+++ js/scriptCarlos.js
@@ -0,0 +1,52 @@
+/* Funciones para ampliar la imagen al pasar por encima y volver a su estado 
+normal al dejar de pasar */
+$(linkFoto).mouseover(() => {
+    $(linkFoto).addClass('animateFont');
+}).mouseout(() => {
+    $(linkFoto).removeClass('animateFont');
+});
+$(linkGaleria).mouseover(() => {
+    $(linkGaleria).addClass('animateFont');
+}).mouseout(() => {
+    $(linkGaleria).removeClass('animateFont');
+});
+$(foto1).mouseover(() => {
+    $(foto1).addClass('animateScale');
+}).mouseout(() => {
+    $(foto1).removeClass('animateScale');
+});
+$(foto2).mouseover(() => {
+    $(foto2).addClass('animateScale');
+}).mouseout(() => {
+    $(foto2).removeClass('animateScale');
+});
+$(foto3).mouseover(() => {
+    $(foto3).addClass('animateScale');
+}).mouseout(() => {
+    $(foto3).removeClass('animateScale');
+});
+$(foto4).mouseover(() => {
+    $(foto4).addClass('animateScale');
+}).mouseout(() => {
+    $(foto4).removeClass('animateScale');
+});
+$(foto5).mouseover(() => {
+    $(foto5).addClass('animateScale');
+}).mouseout(() => {
+    $(foto5).removeClass('animateScale');
+});
+$(foto6).mouseover(() => {
+    $(foto6).addClass('animateScale');
+}).mouseout(() => {
+    $(foto6).removeClass('animateScale');
+});
+$(foto7).mouseover(() => {
+    $(foto7).addClass('animateScale');
+}).mouseout(() => {
+    $(foto7).removeClass('animateScale');
+});
+$(foto8).mouseover(() => {
+    $(foto8).addClass('animateScale');
+}).mouseout(() => {
+    $(foto8).removeClass('animateScale');
+});
